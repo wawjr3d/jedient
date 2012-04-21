dbfca587ffdcdd3d2805d0496f920709877352b8
Wayne Warner <wawjr3d@gmail.com>
2012-04-21 15:37:05 -0400

    add a skin to player controls

--- webapp/gallery-single.php
+++ webapp/gallery-single.php
@@ -15,11 +15,132 @@
 <link href="css/nivo-slider.css" type="text/css" rel="stylesheet">
 
 <script src="thirdparty/flowplayer/flowplayer-3.2.8.min.js"></script>
+<script src="thirdparty/flowplayer/flowplayer.controls-3.2.8.min.js"></script>
 <style type="text/css">
 	#player {
 		width: 400px;
 		height: 122px;
 	}
+	
+	/* root element should be positioned relatively so that
+    child elements can be positioned absolutely */
+#controls {
+    position:relative;
+    height:40px;
+
+    /* black background with a gradient */
+    background:#000 url(images/hulu.png) repeat-x 0 -4px;
+    width:500px;
+}
+
+/* play/pause button */
+#controls .play, #controls .pause {
+    position:absolute;
+    width: 46px;
+    height: 40px;
+    display:block;
+    text-indent:-9999em;
+    background:url(images/hulu.png) no-repeat 10px -61px;
+    cursor:pointer;
+    border-right:1px solid #000;
+}
+
+#controls .play:hover {
+    background-position:10px -105px;
+}
+
+/* pause state */
+#controls .pause {
+    background-position:11px -148px;
+}
+
+#controls .pause:hover {
+    background-position:11px -192px;
+}
+
+/* the timeline (or "scrubber")  */
+#controls .track {
+    left:47px;
+    position:absolute;
+    cursor:pointer;
+    width:285px;
+    border-left:1px solid #999;
+    height:40px;
+}
+
+/* the draggable playhead */
+#controls .playhead {
+    position:absolute;
+    cursor:pointer;
+    background-color:#4ff;
+    opacity:0.3;
+    filter: alpha(opacity=30);
+    width:3px;
+    height:40px;
+    border-right:1px solid #444;
+}
+
+/* buffer- and progress bars. upon runtime the width of these elements grows */
+#controls .progress, #controls .buffer {
+    position:absolute;
+    background-color:#4ff;
+    filter: alpha(opacity=10);
+    opacity:0.1;
+    width:0px;
+    height:40px;
+}
+
+#controls .buffer {
+    background-color:#fff;
+    opacity:0.1;
+    filter: alpha(opacity=10);
+}
+
+/* time display */
+#controls .time {
+    position:absolute;
+    width:129px;
+    left:330px;
+    padding:12px 0;
+    text-align:center;
+    border:1px solid #999;
+    border-width:0 1px;
+    font-size:12px;
+    color:#fff;
+}
+
+/* total duration in time display */
+#controls .time strong {
+    font-weight:normal;
+    color:#666;
+}
+
+/* mute / unmute buttons */
+#controls .mute, #controls .unmute {
+    position:absolute;
+    left:460px;
+    width:40px;
+    height:40px;
+    text-align:center;
+    padding:8px 0;
+    cursor:pointer;
+    text-indent:-9999em;
+    background:url(images/hulu.png) no-repeat 5px -323px;
+}
+
+#controls .mute:hover {
+    background-position:5px -367px;
+}
+
+/* unmute state */
+#controls .unmute {
+    background-position:5px -235px;
+}
+
+#controls .unmute:hover {
+    background-position:5px -279px;
+}
+	
 </style>
 </head>
 <body>
@@ -62,14 +183,9 @@
             <div class="three_fourth last" style="padding:20px 0 0 0;">
               <div class="theme-default">
               	<div id="player"></div>
+              	<div id="controls"></div>
               	<script type="text/javascript">
 $f("player", "thirdparty/flowplayer/flowplayer-3.2.9.swf", {
- 
-    // common clip: these properties are common to each clip in the playlist
-    clip: {
-        // by default clip lasts 5 seconds
-        duration: 5
-    },
  
     // playlist with five entries
     playlist: [
@@ -79,7 +195,7 @@ $f("player", "thirdparty/flowplayer/flowplayer-3.2.9.swf", {
         {url: "images/ad_244-122.jpg", duration: 0},
 
 
-        {url: "media/audio/track26.mp3"}
+        {url: "media/audio/test.mp3"}
 
     ],
  
@@ -88,16 +204,10 @@ $f("player", "thirdparty/flowplayer/flowplayer-3.2.9.swf", {
         audio: {
             url: 'thirdparty/flowplayer/plugins/flowplayer.audio-3.2.8.swf'
         },
-        controls: {
-            playlist: true,
- 
-            // use tube skin with a different background color
-            //url: "http://releases.flowplayer.org/swf/flowplayer.controls-tube-.swf",
-            backgroundColor: '#aedaff'
-        }
+        controls: null
     }
  
-});
+}).controls("controls", {duration: 25});
               	</script>
               
               <!--
