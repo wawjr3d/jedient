45cc62ba014987f505fd5c1cdf61568f0653bce2
Wayne Warner <wawjr3d@gmail.com>
2012-04-21 16:59:40 -0400

    remove "home" dropdown

--- webapp/includes/navigation.php
+++ webapp/includes/navigation.php
@@ -7,14 +7,7 @@
       <li><a href="gallery.php"><strong>Gallery</strong></a></li>
       <li><a href="events.php"><strong>Events</strong></a></li>
       <li><a href="about-us.php"><strong>About Us</strong></a></li>
-      <li class="first"><a href="index.php"><strong>Home</strong></a>
-        <ul style="overflow:visible;">
-          <li><a href="index-full-slider.html">Full Page Slider</a> </li>
-          <li><a href="index.html">Nivo Slider</a></li>
-          <li><a href="index_accordian_slider.html">Accordian</a></li>
-          <li class="last"><a href="index_cu3er_slider.html">CU3ER</a> </li>
-        </ul>
-      </li>
+      <li class="first"><a href="index.php"><strong>Home</strong></a></li>
     </ul>
   </div>
 </div>
