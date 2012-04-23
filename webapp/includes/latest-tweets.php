decc5dcd218a7596af543d3c12c255f00019a495
Wayne Warner <wawjr3d@gmail.com>
2012-04-22 23:40:30 -0400

    filter out tweets we dont want like #jedisomething, try adding a twitter logo

--- webapp/includes/latest-tweets.php
+++ webapp/includes/latest-tweets.php
@@ -1,40 +1,58 @@
 <?php
-		$endpoint = "http://search.twitter.com/search.json";
+	$resource = "http://search.twitter.com/search.json";
+	$hashtag = "#jedi";
+	$tweet_count = isset($howmany_tweets) ? $howmany_tweets : 1;
 		
-		$data = array("q" => "#jedi",
+	$data = array("q" => $hashtag,
               		"result_type" => "recent",
-              		"rpp" => isset($howmany_tweets) ? $howmany_tweets : 1);
+              		"rpp" => 20);
 
-		$request =  implode("?", array($endpoint, http_build_query($data)));
+	$request = implode("?", array($resource, http_build_query($data)));
 
     $session = curl_init($request); 
     curl_setopt($session, CURLOPT_HEADER, false); 
   	curl_setopt($session, CURLOPT_RETURNTRANSFER, true); 
-		$response = curl_exec($session); 
+	$response = curl_exec($session); 
   	curl_close($session); 
   	
   	$json_response = json_decode($response);
   	$tweets = $json_response->results;
-  	
-		if (count($tweets) == 1) {
-				$tweet = $tweets[0];
-				$tweet_id = $tweet->id_str;
-		  	$tweet_created_at = $tweet->created_at;
-		  	$tweet_text = $tweet->text;
+	$filteredTweets = array();
+	$i = 0;
+
+	// filter out tweets not from #jedisomethingelse
+	foreach($tweets as $tweet) {
+		if ($i >= $tweet_count) { break; }
+			
+		if (preg_match("/$hashtag\b(?!-)/i", $tweet->text)) {
+			array_push($filteredTweets, $tweet);
+			$i++;
+		}
+	}
 		  	
-		  	$tweet_link = "http://www.twitter.com";
-		
-		  	echo "<a href='$tweet_link' target='_blank'>$tweet_text</a>";
+	$tweets = $filteredTweets;
+  	
+  	if (count($tweets)) {
+		if ($tweet_count == 1) {
+			$tweet = $tweets[0];
+			$tweet_id = $tweet->id_str;
+			$tweet_created_at = $tweet->created_at;
+			$tweet_text = $tweet->text;
+			  	
+			$tweet_link = "http://www.twitter.com";
+			
+			echo "<a href='$tweet_link' target='_blank'>$tweet_text</a>";
 		} else {
 			echo "<ul>";
-	  	foreach($tweets as $tweet) {
+		  	foreach($tweets as $tweet) {
 				$tweet_id = $tweet->id_str;
-		  	$tweet_created_at = $tweet->created_at;
-		  	$tweet_text = $tweet->text;
-		  	
-		  	$tweet_link = "http://www.twitter.com";
-		
-		  	echo "<li><a href='$tweet_link' target='_blank'>$tweet_text</a></li>";
-	  	}
-	  	echo "</ul>";
+			  	$tweet_created_at = $tweet->created_at;
+			  	$tweet_text = $tweet->text;
+			  	
+			  	$tweet_link = "http://www.twitter.com";
+			
+			  	echo "<li><a href='$tweet_link' target='_blank'>$tweet_text</a></li>";
+		  	}
+		  	echo "</ul>";
 		}
+  	}
--- webapp/js/jedient.js
+++ webapp/js/jedient.js
@@ -3,7 +3,8 @@ $f("music-player", "thirdparty/flowplayer/flowplayer-3.2.9.swf", {
     // playlist with five entries
     playlist: [
 
-        {url: "media/audio/test.mp3"}
+        {url: "media/audio/test.mp3"},
+        {url: "media/audio/test1.mp3"}
 
     ],
  
