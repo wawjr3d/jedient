0520fd809c96762dc7ce341d08f42a143b7b38c8
Wayne Warner <wawjr3d@gmail.com>
2012-04-24 00:32:59 -0400

    add output to cache-tweets script, ignore generated and media info

--- cache-tweets.php
+++ cache-tweets.php
@@ -3,12 +3,17 @@
 	foreach (range(1, 5) as $howmany_tweets) {
 		ob_start();
 	
-		include "webapp/includes/latest-tweets.php";
+		include dirname(__FILE__) . "/webapp/includes/latest-tweets.php";
 	
 		$filecontents = ob_get_contents();
 
 		ob_end_clean();
-	
-		$filename = "webapp/includes/latest-tweets-$howmany_tweets.html";
-		file_put_contents ($filename , $filecontents);
+	 
+		$filename = dirname(__FILE__) . "/webapp/includes/latest-tweets-$howmany_tweets.html";
+		
+		echo "Writing $filename was ";
+		$success = file_put_contents ($filename , $filecontents);
+		echo ($success ? "successful" : "a failure");
+		echo "\n";
+		
 	}
