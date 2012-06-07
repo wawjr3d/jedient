e88cadfb0a2c558375edf2f7f402ca70d1c95619
Regis Bolden <rbolden2@gmail.com>
2012-06-06 22:48:14 -0400

    added new page

---
+++ webapp/artists.php
@@ -0,0 +1,71 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<? $pageTitle = "About Us"; ?>
+	<?php include_once "includes/common-head.php"; ?>
+
+<!--[if IE]>
+        <link rel="stylesheet" type="text/css" href="css/style_ie.css" />
+<![endif]-->
+<!-- Js Files -->
+<script src="library/js/jquery.min.js" type="text/javascript"></script>
+<script src="library/js/custom.js" type="text/javascript"></script>
+</head>
+<body>
+
+<?php include_once "includes/top-menu.php"; ?>
+                    
+<!-- Page Full Container -->
+<div id="page">
+  <!-- Page Middle Container -->
+	<?php include_once "includes/header.php"; ?>
+
+  <div class="cont_round_center">
+    <!-- Content Area - Start Here -->
+    <div class="content">
+      <div class="midarea">
+        <h2 class="intitle"> <span class="title">Artists / Producers</span> <span class="title_text">Jealousy Encourage Driven Intelligence</span> </h2>
+        <div class="middle-inner" id="content">
+          <div class="main_container">
+            <div class="clear"></div>
+            <h3>Klark Kentt Bio</h3>
+			<p><img src="images/jedi_stand.jpg" alt="" height="125" class="alignleft bdr1 heffect" /> Jonathan S. Hughes A.K.A  Klark Kentt is a born businessman from Pittsburgh, PA with a artistic background complete with writing poetry since age 7, performing for crowds since age 12 and writing music since age 16, he has added hip-hop mogul to his list of goals. With his influences in music coming from Jay-Z, Nas, Redman LL Cool J, Styles P and other founding fathers in music he has made himself a stand out artist in Pittsburgh and on the music labels On a Key Records, U.P.S and Square 1. He also has performed at Villanova University, in Allentown, PA, Faded Fest in Pittsburgh, Proud to Be Local and University of Pittsburgh just to name a few. With his hard earned degree from Villanova Univeristy in marketing and his experience in the entertainment industry Jonathan has begun a journey to start J.E.D.I Entertainment with the hopes of making it Pittsburgh's heaviest hitting record label. As part of a formidable business network and the guidance of very successful mentors he vows to be the driving force of change in music and business in the21st century. Seeing is believing, impossible is nothing and the future is now. You are witnessing the writing of history, 2012 is not the end its just the beginning, of 2Ks.</p>
+			<h3>Shout Out</h3>
+			<p>It's ya Boy Klark Kentt A.K.A Two Kays! I'd liked to give my shout outs to the whole east side of Pittsburgh, all the way from Braddock to East Liberty. Shout out to the West End, North Side and the South Side. Shout out to all the artist doing it the same.</p>
+			<p>Shout out personally to those who help J.E.D.I and myself in our current journey:<br />On A Key Records, Phocused Recordsin Philadelphia, U.P.S Musik Group, Y.M DA Kid, King Pinz Records, Jenesis Magazine, Rock Around The World Productions, 700, Owey, BTP, Button Pusha,Mista Scrap and Statement Records. , Square One Records, It Is What It Is Ent.,Blackberry Studios, Tefnology, Heru, Poppy, H.A.W.G, Charlie Vangogh, D. Lane, DMB (out of Detroit) and so many more.</p>
+			<p>I just want to say anyone from this city, who knows what it is to grow up here, struggle here, grind here, try to build here, should know its no easy task. But, the reality is nothing in life is easy. "So if you really 'bout that action mount up", Pittsburgh is here and the door is open. P.S. Shout outs to my people in Atlanta, New York, Chicago, Philadelphia, South Carolina, North Carolina, Florida, California, New Jersey, Allentown, Reading, Akron, Toledo, Cleveland, Columbus, West Virginia, Louisiana, Detroit, Delaware and if I did not shout out your city or state hit me up.</p>
+			<p>Keep ya eyes to the sky...</p>
+			
+			<!--<div class="specialp1"> Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.  Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. </div>
+            Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.
+            <p></p>
+            <blockquote>
+              <div class="quoteb"> Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae. </div>
+            </blockquote>
+            <p>Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. </p>
+            <ul class="list_style_01">
+              <li>Vestibulum tortor quam, feugiat vitae</li>
+              <li>Vestibulum tortor quam, feugiat vitae</li>
+              <li>Vestibulum tortor quam, feugiat vitae</li>
+              <li>Vestibulum tortor quam, feugiat vitae</li>
+            </ul>
+            <p>Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.  Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. </p>
+	-->
+		  
+	</div>
+          
+          <?php include_once "includes/sidebar.php"; ?>
+        </div>
+        <div class="clear"></div>
+        <div class="clear"></div>
+      </div>
+    </div>
+    <!-- Content Area - End Here -->
+  </div>
+
+	<?php include_once "includes/footer.php"; ?>
+  <div class="clear"></div>
+</div>
+<?php include_once "includes/common-foot.php"; ?>
+</body>
+</html>
