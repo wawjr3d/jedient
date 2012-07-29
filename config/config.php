ac3a5e420e6c0df4bbed359b0b2f51844c1b0ee5
Wayne Warner <wawjr3d@gmail.com>
2012-07-29 03:31:07 -0400

    autoload classes relative to the location of the config file
    the php in this app may execute from different depths in a directory
    structure, autoloading relative to the config location means we can drop
    this folder anywhere and the app will still be able to find our classes

--- config/config.php
+++ config/config.php
@@ -3,9 +3,9 @@
 	error_reporting(E_ALL);
 	putenv("TZ=US/Eastern");
 
-	define("ROOT_DIR", "/jedient/");
+	define("ROOT_DIR", dirname(__FILE__) . "../../");
 	define("CLASSES_DIR", ROOT_DIR . "classes");
 
 	function __autoload($class_name) {
-		require_once $_SERVER['DOCUMENT_ROOT'] . CLASSES_DIR . "/$class_name.php";
+		require_once CLASSES_DIR . "/$class_name.php";
 	}
